a5505e32a368bbd150dbcc15663b9aeb4a1ccdf6
Silvia <syl_peke14@hotmail.com>
2023-04-28 11:16:31 -0500

    Add more conditionals examples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -12,3 +12,22 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 
 
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+const temperature = 30;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
